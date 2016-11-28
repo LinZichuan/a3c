724a960d0a8e6a2f7ef0b967774d3b2b57ed1046
LinZichuan <linzichuan12@163.com>
2016-11-28 13:46:05 +0800

    Fixup loss.

--- agent.py
+++ agent.py
@@ -17,7 +17,7 @@ class Agent:
         self.width, self.height = 84,84
         self.hist_len = 4
         self.learning_rate = 0.0001 #0.00025
-        self.thread_num = 8
+        self.thread_num = 16
         self.env_name = 'Breakout-v0'
         self.config = config
         self.gamma = 0.99
@@ -35,26 +35,19 @@ class Agent:
         self.R_t_place = tf.placeholder('float', [None])
         self.a_t_place = tf.placeholder('float', [None, self.num_actions])
 
-        log_pi = tf.log(self.action_probs + 1e-6)
-        entropy = - tf.reduce_sum(self.action_probs * log_pi, reduction_indices=1)
-        td = self.R_t_place - self.state_value
-        policy_loss = - tf.reduce_sum(tf.reduce_sum(tf.mul(log_pi, self.a_t_place), reduction_indices=1) * td + entropy * 0.01)
-        value_loss = 0.5 * tf.nn.l2_loss(self.R_t_place - self.state_value)
-        self.total_loss = policy_loss + value_loss
-        '''
-        log_prob = tf.log(tf.reduce_sum(tf.mul(self.action_probs, self.a_t_place), reduction_indices=1))
-        p_loss = -log_prob * (self.R_t_place - self.state_value)
-        xentropy_loss = tf.reduce_sum(self.action_probs * log_prob, name='xentropy_loss')
-        v_loss = tf.reduce_mean(tf.square(self.R_t_place - self.state_value))
-        self.total_loss = p_loss + 0.01 * xentropy_loss + v_loss
-        self.loss = tf.reduce_mean(self.total_loss)
-        '''
-        '''
-        #log_probs = tf.log(self.action_probs  + 1e-6)
-        #log_pi_a_given_s = tf.reduce_sum(log_probs * tf.one_hot(self.a_t_place, self.num_actions), 1)
-        #p_loss = tf.reduce_sum(log_pi_a_given_s * (self.R_t_place - self.state_value))
-        #v_loss = tf.nn.l2_loss(self.state_value - self.R_t_place)
-        '''
+        log_probs = tf.log(self.logits + 1e-6)
+        log_pi_a_given_s = tf.reduce_sum(log_probs * self.a_t_place, 1)
+        advantage = tf.sub(tf.stop_gradient(self.value), self.R_t_place, name='advantage')
+        policy_loss = tf.reduce_sum(log_pi_a_given_s * advantage, name='policy_loss')
+        xentropy_loss = tf.reduce_sum(
+                                self.logits * log_probs, name='xentropy_loss')
+        value_loss = tf.nn.l2_loss(self.value - self.R_t_place, name='value_loss')
+        pred_reward = tf.reduce_mean(self.value, name='predict_reward')
+        self.total_loss = tf.add_n([policy_loss, xentropy_loss * 0.01, value_loss])
+        self.total_loss = tf.truediv(self.total_loss,
+                tf.cast(tf.shape(self.R_t_place)[0], tf.float32),
+                name='total_loss')
+
         self.minimize = optimizer.minimize(self.total_loss)
         return self.state, self.a_t_place, self.R_t_place, self.minimize
     
@@ -74,39 +67,10 @@ class Agent:
 
         l = Linear('linear1', l, out_dim=512, nl=tf.identity)
         #l = tf.nn.relu(l, name='relu')
-        self.action_probs = tf.nn.softmax(Linear('fc-pi', l, out_dim=self.num_actions, nl=tf.identity))
-        self.state_value = Linear('fc-v', l, 1, nl=tf.identity)
-        '''
-        w1 = tf.get_variable('w1', [8,8,self.hist_len,16], initializer=init)
-        b1 = tf.get_variable('b1', [16], initializer=init)
-        conv1 = tf.nn.conv2d(self.state, w1, [1,1,4,4], 'VALID', data_format=data_format)
-        bias1 = tf.nn.bias_add(conv1, b1, data_format)
-        out1 = tf.nn.relu(bias1)
+        self.logits = tf.nn.softmax(Linear('fc-pi', l, out_dim=self.num_actions, nl=tf.identity))
+        self.value = Linear('fc-v', l, 1, nl=tf.identity)
 
-        w2 = tf.get_variable('w2', [4,4,16,32], initializer=init)
-        b2 = tf.get_variable('b2', [32], initializer=init)
-        conv2 = tf.nn.conv2d(out1, w2, [1,1,2,2], 'VALID', data_format=data_format)
-        bias2 = tf.nn.bias_add(conv2, b2, data_format)
-        out2 = tf.nn.relu(bias2)
 
-        shape = out2.get_shape().as_list()
-        out2_flat = tf.reshape(out2, [-1, reduce(lambda x,y: x*y, shape[1:])])
-        shape = out2_flat.get_shape().as_list()
-
-        w3 = tf.get_variable('w3', [shape[1], 256], initializer=init)
-        b3 = tf.get_variable('b3', [256], initializer=init)
-        bias3 = tf.nn.bias_add(tf.matmul(out2_flat, w3), b3)
-        out3 = tf.nn.relu(bias3)
-
-        w4 = tf.get_variable('w4', [256, self.num_actions], initializer=init)
-        b4 = tf.get_variable('b4', [self.num_actions], initializer=init)
-        logits = tf.nn.bias_add(tf.matmul(out3, w4), b4)
-        self.action_probs = tf.nn.softmax(logits)
-
-        w5 = tf.get_variable('w5', [256, 1], initializer=init)
-        b5 = tf.get_variable('b5', [1], initializer=init)
-        self.state_value = tf.nn.bias_add(tf.matmul(out3, w5), b5)
-        '''
     def sample_policy_action(self, num_actions, probs):
         """
         Sample an action from an action probability distribution output by
@@ -149,7 +113,7 @@ class Agent:
             r_batch = []
             while not terminal and (t - t_start) != t_max:
                 #use policy network to choose a_t
-                probs = self.sess.run(self.action_probs, feed_dict={self.state: [np.transpose(history, (1,2,0))]})[0] ##TODO
+                probs = self.sess.run(self.logits, feed_dict={self.state: [np.transpose(history, (1,2,0))]})[0] ##TODO
                 action_index = self.sample_policy_action(self.num_actions, probs)
                 a_t = np.zeros([self.num_actions]) #TODO
                 a_t[action_index] = 1
@@ -157,6 +121,7 @@ class Agent:
                 a_batch.append(a_t)
                 #get reward and s_t_plus_1
                 s_t_plus_1, r_t, terminal = env.act(action_index)
+                #env.env.render()
                 ep_reward += r_t
                 history[:-1] = history[1:]
                 history[-1] = s_t_plus_1
@@ -166,7 +131,7 @@ class Agent:
                 T += 1
                 ep_t += 1
                 s_t = s_t_plus_1
-            R_t = 0 if terminal else self.sess.run(self.state_value, feed_dict={self.state: [np.transpose(history, (1,2,0))]})[0][0] #V(s_t_plus_1)
+            R_t = 0 if terminal else self.sess.run(self.value, feed_dict={self.state: [np.transpose(history, (1,2,0))]})[0][0] #V(s_t_plus_1)
             R_batch = np.zeros(t)
             for i in xrange(t-1, t_start): #from back to front
                 R_t = r_batch[i] + self.gamma * R_t
@@ -186,7 +151,6 @@ class Agent:
                 loss_eval = 0
 
             if terminal:
-                #self.sess.run()
                 s_t, _, _, _ = env.newRandomGame()
                 terminal = False
                 print "THREAD:", thread_id, "/ TIME:", T, "/ REWARD:", ep_reward
